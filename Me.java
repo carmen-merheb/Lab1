3bd0ca5396c6bdbcd34add8604f15b0481fe9ff6
Carmen Merheb <carmen.merheb@lau.edu>
2023-01-25 21:02:27 +0200

    Revert "My year of study added"
    This reverts commit fde0964318a682e50af4ac8e87ed6270e06a7733.

--- Me.java
+++ Me.java
@@ -2,6 +2,5 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("Carmen Merheb");
 		System.out.println("Computer Engineering");
-		System.out.println("First year of study");
 	}
 }
