fde0964318a682e50af4ac8e87ed6270e06a7733
Carmen Merheb <carmen.merheb@lau.edu>
2023-01-25 20:44:08 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me{
 	public static void main(String[] args){
 		System.out.println("Carmen Merheb");
 		System.out.println("Computer Engineering");
+		System.out.println("First year of study");
 	}
 }
